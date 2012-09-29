0cc3d89d6b15644772933acc6740020cb2dec29f
Patric Nordmark <patric.nordmark@gmail.com>
2012-09-29 02:47:54 +0200

    Problem 10

---
+++ problem_010.js
@@ -0,0 +1,25 @@
+/**
+ * Problem 10
+ * http://projecteuler.net/problem=10
+ *
+ * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+ * Find the sum of all the primes below two million.
+ */
+var p = function (n) {
+  var p, i,
+      size = n / 2 - 1,
+      d = new Array(size),
+      sum = 2;
+
+  for (i = 0, p = 3; p < n; p += 2, i++) d[i] = p;
+
+  for (p = 0; p < size; p++) {
+    if (d[p] === 0) continue;
+    sum += d[p];
+    for (i = p + d[p]; i < size; i += d[p]) d[i] = 0;
+  }
+
+  return sum;
+};
+
+console.log(p(2000000));
